409d9acf8cccd1cbc780478e8aea22a595c8770f
Zhu Ming <mingzhu.z+github@gmail.com>
2014-11-18 10:58:43 +0800

    [zh_CN] force to use sphinx 1.2 since build with sphinx 1.3 meets error

--- _project/vagrant/provision.sh
+++ _project/vagrant/provision.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Installing sphinx..."
+echo "update apt repository..."
 apt-get update >/dev/null 2>&1
+echo "Installing python-setuptools..."
 sudo apt-get install -y python-setuptools >/dev/null 2>&1
+echo "Installing easy_install..."
 sudo apt-get install -y easy_install >/dev/null 2>&1
-sudo easy_install sphinx >/dev/null 2>&1
+echo "Installing sphinx..."
+sudo easy_install sphinx==1.2 >/dev/null 2>&1
+echo "Installing sphinxcontrib-phpdomain..."
 sudo easy_install sphinxcontrib-phpdomain >/dev/null 2>&1
