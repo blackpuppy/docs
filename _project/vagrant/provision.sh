ec9cbf897822789ff01b39f8223a1f69da364290
Zhu Ming <mingzhu.z+github@gmail.com>
2015-08-31 19:26:35 +0800

    Vagrant config
    - install Travis CI Command Line Client

--- _project/vagrant/provision.sh
+++ _project/vagrant/provision.sh
@@ -8,34 +8,68 @@ echo "**** export locale environment variables for root..."
 sed -i '$a \
 \
 export LC_ALL=en_US.UTF-8\
-export LANG=en_US.UTF-8' ~/.profile
+export LANG=en_US.UTF-8
+' ~/.profile
 source ~/.profile
 
 echo "**** export locale environment variables for vagrant..."
 sed -i '$a \
 \
+SSH_ENV="$HOME/.ssh/environment"\
+\
+function start_agent {\
+	echo "Initialising new SSH agent..."\
+	/usr/bin/ssh-agent | sed \x27s/^echo/#echo/\x27 > "${SSH_ENV}"\
+	echo succeeded\
+	chmod 600 "${SSH_ENV}"\
+	. "${SSH_ENV}" > /dev/null\
+	/usr/bin/ssh-add;\
+}\
+\
+# Source SSH settings, if applicable\
+\
+if [ -f "${SSH_ENV}" ]; then\
+	. "${SSH_ENV}" > /dev/null\
+	#ps ${SSH_AGENT_PID} doesn not work under cywgin\
+	ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {\
+		start_agent;\
+	}\
+else\
+	start_agent;\
+fi\
+\
 export LC_ALL=en_US.UTF-8\
 export LANG=en_US.UTF-8\
 \
-cd /docs
+cd ~/docs\
 ' /home/vagrant/.profile
 
 
-echo "**** Installing vim..."
-sudo apt-get -y install vim
-
-
 echo "**** update apt repository..."
-sudo apt-get update >/dev/null 2>&1
+apt-get update >/dev/null 2>&1
+
+echo "**** Installing vim..."
+apt-get -y install vim >/dev/null 2>&1
 
 echo "**** Installing python-setuptools..."
-sudo apt-get install -y python-setuptools >/dev/null 2>&1
+apt-get install -y python-setuptools >/dev/null 2>&1
 
 # echo "**** Installing easy_install..."
-# sudo apt-get install -y easy_install >/dev/null 2>&1
+# apt-get install -y easy_install >/dev/null 2>&1
 
 echo "**** Installing sphinx..."
-sudo easy_install sphinx==1.2.3 >/dev/null 2>&1
+easy_install sphinx==1.2.3 >/dev/null 2>&1
 
 echo "**** Installing sphinxcontrib-phpdomain..."
-sudo easy_install sphinxcontrib-phpdomain >/dev/null 2>&1
+easy_install sphinxcontrib-phpdomain >/dev/null 2>&1
+
+echo "**** Installing Travis CI Command Line Client..."
+# apt-get install -y python-software-properties software-properties-common
+# apt-add-repository -y ppa:brightbox/ruby-ng
+# apt-get update
+# apt-get install -y ruby2.1 ruby-switch ruby2.1-dev
+# ruby-switch --set ruby2.1
+# gem update --system
+
+apt-get install -y ruby-full gem
+gem install travis
